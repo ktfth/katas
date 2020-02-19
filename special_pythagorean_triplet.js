7e2b2086b25fb969bf8504ac867aeef92910438e
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-02-19 15:48:22 -0300

    Special Pythagorean Triplet

--- special_pythagorean_triplet.js
+++ special_pythagorean_triplet.js
@@ -31,19 +31,20 @@ function generateSequence(n=0, m=10, s=1) {
 assert.deepEqual(generateSequence(1, 4), [1, 2, 3]);
 assert.deepEqual(generateSequence(2, 6, 2), [2, 4]);
 
-function triplet() {
+function triplet(p) {
   let out = {};
   let gs = generateSequence(2, 550, 2);
   for (let i = 0; i < gs.length; i += 1) {
     let r = gs[i];
     let st = r * r / 2;
-    divisors(st).reduce((s, t) => {
+    divisors(st).map((s) => {
+      let t = divisors(st).length;
       let x = (r + s) + (r + t) + (r + s + t);
       if (parseInt(x) > 0) {
-        out[x] = (r + s) + (r + t) + (r + s + t);
+        out[x] = (r + s) * (r + t) * (r + s + t);
       }
     });
   }
-  return out;
+  return out[p] !== undefined ? out[p] : {};
 }
 console.log(triplet(1000));
