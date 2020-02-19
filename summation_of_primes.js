10494a3aa24c837bb69fddca91edace16d5568ba
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-02-19 19:33:19 -0300

    Summation of primes

---
+++ summation_of_primes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const assert = require('assert');
+
+function primes(n) {
+  let primes = [2];
+  let attempt = 3;
+  while (primes.length < n) {
+    for (let i = 0; i < primes.length; i += 1) {
+      if (attempt % primes[i] != 0 && primes.indexOf(attempt) === -1) {
+        primes.push(attempt);
+      } else {
+        break;
+      }
+    }
+    attempt += 2;
+  }
+  return primes;
+}
+console.log(primes(10).slice(0, 4).reduce((a, b) => a + b));
