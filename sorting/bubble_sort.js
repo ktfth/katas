b505947f699ee0cd9b2fc1e42274612a24159e07
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-02-26 21:23:54 -0300

    Bubble sort

---
+++ sorting/bubble_sort.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const assert = require('assert');
+
+function swap(arr, i, j) {
+  let t = arr[i];
+  arr[i] = arr[j];
+  arr[j] = t;
+}
+
+function bubbleSort(arr) {
+  var swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i] && arr[i + 1] && arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+assert.deepEqual(bubbleSort([4, 3, 5, 2, 1].slice()), [1, 2, 3, 4, 5]);
